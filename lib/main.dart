d8d90a71f22eaaf8bff96d9a814153734bffe098
Elmira <shainurova.nursultan@gmail.com>
2025-12-06 21:29:22 +0500

    feat: add the starting screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'screens/start_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const FinTrackApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class FinTrackApp extends StatelessWidget {
+  const FinTrackApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'FinTrack',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
       ),
+      home: const StartScreen(),
     );
   }
 }
