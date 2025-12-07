79dc2b3266196366b590f73ff5e1c1b6c856956d
Elmira <shainurova.nursultan@gmail.com>
2025-12-07 17:39:01 +0500

    fix: update category and date pickers

--- lib/screens/home/widgets/category_dropdown.dart
+++ lib/screens/home/widgets/category_dropdown.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fintrack_app/components/fields/input_field.dart';
 
-class CategoryDropdown extends StatelessWidget {
+class CategoryDropdown extends StatefulWidget {
   final String current;
   final VoidCallback onTap;
 
@@ -12,11 +12,47 @@ class CategoryDropdown extends StatelessWidget {
   });
 
   @override
+  State<CategoryDropdown> createState() => _CategoryDropdownState();
+}
+
+class _CategoryDropdownState extends State<CategoryDropdown> {
+  TextEditingController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _updateController();
+  }
+
+  @override
+  void didUpdateWidget(CategoryDropdown oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.current != widget.current) {
+      _updateController();
+    }
+  }
+
+  void _updateController() {
+    final bool isHint = widget.current == "Select category";
+    _controller?.dispose();
+    _controller = isHint ? null : TextEditingController(text: widget.current);
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final bool isHint = widget.current == "Select category";
+    
     return InputField(
-      hint: current,
+      hint: isHint ? widget.current : "",
+      controller: _controller,
       readOnly: true,
-      onTap: onTap,
+      onTap: widget.onTap,
       suffixIcon: const Icon(Icons.keyboard_arrow_down),
     );
   }
