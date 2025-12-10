8b725565914bdf73cc7c77eabe91395a9815aa85
Elmira <shainurova.nursultan@gmail.com>
2025-12-11 00:52:37 +0500

    feat: ui improvements

--- lib/screens/home/widgets/modals/currency_modal.dart
+++ lib/screens/home/widgets/modals/currency_modal.dart
@@ -4,7 +4,7 @@ import 'package:fintrack_app/components/modal/animated_selectable_tile.dart';
 import 'package:fintrack_app/utils/currency.dart';
 import 'package:fintrack_app/utils/app_localizations.dart';
 
-class CurrencyModal extends StatelessWidget {
+class CurrencyModal extends StatefulWidget {
   final Currency selectedCurrency;
   final Function(Currency) onSelected;
 
@@ -14,12 +14,17 @@ class CurrencyModal extends StatelessWidget {
     required this.onSelected,
   });
 
+  @override
+  State<CurrencyModal> createState() => _CurrencyModalState();
+}
+
+class _CurrencyModalState extends State<CurrencyModal> {
   IconData _getCurrencyIcon(Currency currency) {
     switch (currency) {
       case Currency.dollar:
         return Icons.attach_money;
       case Currency.tenge:
-        return Icons.currency_ruble;
+        return Icons.currency_exchange; // Using currency_exchange as tenge icon
       case Currency.euro:
         return Icons.euro;
     }
@@ -38,15 +43,95 @@ class CurrencyModal extends StatelessWidget {
         child: ListView(
           shrinkWrap: true,
           children: Currency.values.map((currency) {
+            // For tenge, show the symbol as text instead of icon
+            if (currency == Currency.tenge) {
+              return _TengeSelectableTile(
+                title: '${currency.name} (${currency.symbol})',
+                symbol: currency.symbol,
+                onTap: () {
+                  widget.onSelected(currency);
+                  Navigator.pop(context);
+                },
+              );
+            }
             return AnimatedSelectableTile(
               title: '${currency.name} (${currency.symbol})',
               icon: _getCurrencyIcon(currency),
               onTap: () {
-                onSelected(currency);
+                widget.onSelected(currency);
                 Navigator.pop(context);
               },
             );
           }).toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class _TengeSelectableTile extends StatefulWidget {
+  final String title;
+  final String symbol;
+  final VoidCallback onTap;
+
+  const _TengeSelectableTile({
+    required this.title,
+    required this.symbol,
+    required this.onTap,
+  });
+
+  @override
+  State<_TengeSelectableTile> createState() => _TengeSelectableTileState();
+}
+
+class _TengeSelectableTileState extends State<_TengeSelectableTile> {
+  bool isPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final isDark = theme.brightness == Brightness.dark;
+
+    return GestureDetector(
+      onTapDown: (_) => setState(() => isPressed = true),
+      onTapCancel: () => setState(() => isPressed = false),
+      onTapUp: (_) => setState(() => isPressed = false),
+      onTap: widget.onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 140),
+        curve: Curves.easeOut,
+        padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 12),
+        decoration: BoxDecoration(
+          color: isPressed
+              ? (isDark ? Colors.white.withOpacity(0.1) : Colors.grey.shade200)
+              : Colors.transparent,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          children: [
+            Container(
+              width: 22,
+              height: 22,
+              alignment: Alignment.center,
+              child: Text(
+                widget.symbol,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  color: isDark ? Colors.white70 : Colors.black87,
+                ),
+              ),
+            ),
+            const SizedBox(width: 16),
+            Text(
+              widget.title,
+              style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.w500,
+                color: isDark ? Colors.white : Colors.black,
+              ),
+            ),
+          ],
         ),
       ),
     );
